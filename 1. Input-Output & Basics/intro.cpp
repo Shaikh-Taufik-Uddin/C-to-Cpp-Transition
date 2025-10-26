1bd1db354e50ea26d6ff8206081389584091060b
Shaikh Taufik Uddin <shaikhtaufiku@gmail.com>
2025-10-26 12:33:01 +0530

    Delete 1. Input-Output & Basics directory

--- 1. Input-Output & Basics/intro.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout << "hello world" << endl << "this is next line\nanother line";
-    // std::cout << "hello world";
-    return 0;
-}
--- 1. Input-Output & Basics/output.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int x, y;
-    cin >> x >> y;
-    cout << "value of x and y is " << x << " and " << y;
-    return 0;
-}
